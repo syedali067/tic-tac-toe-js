c26c48d03ba2560f7b1ae5b22e715adfed91e6e6
syedali067 <as7073085@gmail.com>
2026-04-28 20:52:04 +0500

    Added screenshot

--- README.d
+++ README.d
@@ -12,12 +12,12 @@
 
 ## ## 📸 Screenshots
 
-<img src="Picture1.png">
-<img src="Picture2.png">
+![Game Start](images/picture1.png)
+![Winning State](images/picture2.png)
 
 
 ## 🔗 Live Demo
 <<<<<<< HEAD
 =======
 https://syedali067.github.io/tic-tac-toe-js/
->>>>>>> 4836fe688bb6e44ed2adbf157bcbf8397d215475
+
